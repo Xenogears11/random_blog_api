6cfe17012e1002e92eb933250060a12648b1ff18
Xenogears11 <agent0092x@gmail.com>
2017-09-06 12:29:53 +0300

    added create_tables

--- api/database/database.py
+++ api/database/database.py
@@ -7,6 +7,6 @@ Base = declarative_base()
 with open('res/connect_str.txt', 'r', encoding = 'utf-8') as file:
     line = file.read()
     connect_str = line.rstrip()
-    
+
 engine = create_engine(connect_str)
 DBSession = sessionmaker(bind = engine)
